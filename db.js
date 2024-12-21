ad0ac5d7d1d442999c0c61630e8067cb1c6b70d9
KUNDANYADAV7 <kdyd999@gmail.com>
2024-12-21 10:39:35 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Define the MongoDB Connection URL
-const  mongoURL = 'mongodb://localhost:27017/ht'
+// const  mongoURL = process.env.MONGODB_URL_LOCAL
+const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL,{
--- server2.js
+++ server2.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); //store data in req.body
@@ -16,4 +19,5 @@ app.use('/menu',menuRoutes);
 
 
 
-app.listen(3000,()=>console.log('Listening on port 3000'));
+
+app.listen(PORT,()=>console.log('Listening on port 3000'));
